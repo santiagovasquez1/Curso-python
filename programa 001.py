e5a25624cc6faba0210437982d525c503de62eba
svasquez <svasquez@controldiseno.com>
2019-09-15 09:20:40 -0500

    Primeros pasos en python

---
+++ programa 001.py
@@ -0,0 +1,12 @@
+print "Bienvenidos a Python"
+print 'Uso de comilla sencilla'
+print "Esto 'permite' mayor libertad"
+
+#Uso de comentario de una sola linea
+print "Hola todos"
+
+""" Aqui otro comentario
+pero usando varias lineas
+"""
+
+print "Despues de los comentarios"
---
+++ programa 003.py
@@ -0,0 +1,42 @@
+#+-*/%**//
+
+#Operaciones aritmeticas
+print "Suma" , 3+5
+print 3.4+7.8
+print 4+5.6
+print "-----"
+
+#Resta
+print "Resta", 2-7
+print  6.5-3.2
+print  12-5.6
+print "-----"
+
+print "Multiplicacion" , 3.*4
+print 3.2*6.7
+print 4*5.6
+print "------"
+
+#Si los operandos son int es floor division
+#Si los operandos no son int es division verdadera
+print "Division" , 6./2.
+print 4.3/2.1
+print 7/3
+print 7.0/3
+print 7/3.0
+print "------"
+print "Modulo", 6%2
+print 7%2
+print 7.5%2.1
+print "------"
+
+#Exponente
+print "Exponente", 5**3
+print 2.3**2.1
+print 3.9**2
+print "------"
+
+#Floor division sirve para aproximar a numero entero
+print "Floot", 5//2
+print 5.0//2.0
+print 5.0//2
